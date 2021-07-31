317382ccb7f9d48b4c1f3dd925963bbab34a05e5
Lydever <65069676+Lydever@users.noreply.github.com>
2021-07-31 11:30:03 +0800

    Update Editor.vue

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -5,7 +5,7 @@
         style="background: #6A60E3"
         mode="horizontal"
       >
-        <div style="width: 400px; float: left;height:70px;color: #fff; background: #5951B6;">
+        <div style="width: 400px; float: left;height:70px;color: #fff; background: #5951B6;line-height: 70px">
           <h1 style="text-align: center">{{ templatesTitle }}</h1>
         </div>
         <el-form inline="inline" style="float: right; width: 1000px" >
